99703138638ec80a5a93a36e671c4fb4c3e2f80c
2CGuimel54 <00001104043865sp@al.educacao.sp.gov.br>
2024-11-06 08:20:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+///Miguel Isaac Vasconcelos Gomes 2ºC///
+
+criaCartao(
+    'Arte',
+    'Quem foi que descobriu a música?',
+    'A primeira pessoa a registrar em papel uma música foi Qiugong, no século 6. Esse chinês tocava uma espécie de cítara e teria transcrito a peça Jieshi Diao Youlan.'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual gênero de jogos é o mais jogado?',
+    'Baseado na quantidade de jogadores, os gêneros de jogos mais populares atualmente são Battle Royale, MOBA e RPG. Jogos destes gêneros somam bilhões de jogadores em todo o mundo'
+
+)
+
+criaCartao(
+    'Programação',
+    'Você sabe o que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Livros',
+    'Qual é o 3° livro mais famoso do mundo?',
+    'O 3° livro mais famoso e lido do mundo é o Harry Potter, de J. K. Rowling.'
+)
