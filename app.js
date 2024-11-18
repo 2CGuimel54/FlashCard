dfadc62a3179c7d081bc1684edf97f527ffc8ccd
2CGuimel54 <00001104043865sp@al.educacao.sp.gov.br>
2024-11-18 09:02:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-///Miguel Isaac Vasconcelos Gomes 2ºC///
+///Miguel Isaac Vasconcelos Gomes 2ºC 07/11/2024///
 
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
